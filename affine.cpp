d88289ae11240af95c0e861bd211918473b9cea1
Luke Fraser <totalvideopro@gmail.com>
2013-02-24 15:27:32 -0800

    add affine

---
+++ affine.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <stdlib.h>
+#include "pgmImage.h"
+#include <iostream>
+
+using namespace std;
+
+struct imageMeta{
+	char * file;
+	int 
+};
+
+
+int main( int argc, char ** argv ){
+	if( argc != 2 ){
+		cout << "-Error: wrong number of arguments-" << endl;
+		return -1;
+	}
+	const char * file = argv[1];
+	char **
+
+	// //SUDO//
+	// file = open(imageCoordsFile);
+	// while(file is good){
+	// 	imageCoordsList.append(file.getline(maxSize, ','));
+	// }
+
+	// for c in imageCoordsList:
+	// 	image.append(new image(c[0]))
+
+	// for image in images:
+	// 	resimage.append(image.affine())
+}
